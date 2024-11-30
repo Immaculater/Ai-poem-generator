c0229748b5209baef31d7f2b03bafd8530969dee
Immaculater <immaculaterbayero@gmail.com>
2024-11-30 13:45:37 +0100

    added slight change to the previous commit

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let prompt = `User instruction: Generate a short poem about ${userInstructionElement.value}`;
 
   let context =
-    "you are a powerful and knowledgeable AI, that can generate lovely poems about anything, Your mission is to generate a 4 line poem that relates to the user instructions in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the translated poem with 'SheCodes AI' inside a <strong> element which must begin after a <br /> and NOT at the beginning. and translate the generated poem in french, without 'SheCodes AI' sign. there should be a <br /> <br /> between the original poem and the translated version. the translated version should be in <em>";
+    "you are a powerful and knowledgeable AI, that can generate lovely poems about anything, Your mission is to generate a 4 line poem that relates to the user instructions in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. and translate the generated poem in french. there should be a <br /> <br /> between the original poem and the translated version. the translated version should be in <em>, Sign the translated poem with 'SheCodes AI' inside a <strong> element, and it must be after a <br /> element and NOT at the beginning";
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayPoem);
